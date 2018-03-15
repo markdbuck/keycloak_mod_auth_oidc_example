08907e19dbef0e496fb569bb5de69466c4b7d0c9
mark buck <mark_buck@ao.uscourts.gov>
2018-03-15 12:41:35 -0400

    debugging corrections

--- html/protected/index.php
+++ html/protected/index.php
@@ -3,6 +3,7 @@
     <h1>Hello, <?php echo($_SERVER['REMOTE_USER']) ?></h1>
     <pre><?php print_r(apache_request_headers()); ?></pre>
     <a href="../">back to index</a>
-    <a href="/protected/redirect_uri?logout=<?php echo ("http" . (isset($_SERVER['HTTPS']) ? 's' : '') . "://". $_SERVER['HTTP_HOST'] . ":" . $_SERVER['SERVER_PORT'] ."/") ?>">Logout</a>
+    <a href="/protected/redirect_uri?logout=<?php echo ("http" . (isset($_SERVER['HTTPS']) ? 's' : '') . "://". $_SERVER['HTTP_HOST'] . "/") ?>">Logout</a>
   </body>
 </html>
+~
