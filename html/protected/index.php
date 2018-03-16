47cdc9b14cd4c0a298ba0eb29ac05f2028cfb7ff
mark buck <mark_buck@ao.uscourts.gov>
2018-03-16 07:20:53 -0400

    fix authorization

--- html/protected/index.php
+++ html/protected/index.php
@@ -2,6 +2,7 @@
   <body>
     <h1>Hello, <?php echo($_SERVER['REMOTE_USER']) ?></h1>
     <pre><?php print_r(apache_request_headers()); ?></pre>
+    <a href="restricted/index.php">Access mod_oidc restricted page</a><br/>
     <a href="../">back to index</a>
     <a href="/protected/redirect_uri?logout=<?php echo ("http" . (isset($_SERVER['HTTPS']) ? 's' : '') . "://". $_SERVER['HTTP_HOST'] . "/") ?>">Logout</a>
   </body>
